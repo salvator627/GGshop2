ba2ba0f7bd40188f3da7c11e3d49cdbaeaa6869d
Salvator <salvator926@gmail.com>
2022-11-02 18:19:31 +0700

    front-end update

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,10 +1,28 @@
-<footer>
+<div class="foot">
     <hr>
-    <div class="container">
-        <div class="row">
-            <div class="col text-center">
-                Copyright @2022 GGSHOP
-            </div>
-        </div>
+   <section class="footer">
+    <div class="social">
+        <a href="#"><i class="fab fa-instagram"></i></a>
+        <a href="#"><i class="fab fa-facebook-f"></i></a>
+        <a href="https://github.com/salvator627/GGshop2"><i class="fab fa-github"></i></a>
+        <a href="#"><i class="fab fa-twitter"></i></a>
     </div>
-</footer>
+    <ul class="list">
+        <li>
+            <a href="#">FAQ</a>
+        </li>
+        <li>
+            <a href="#">Services</a>
+        </li>
+        <li>
+            <a href="#">About</a>
+        </li>
+        <li>
+            <a href="#">Privacy Policy</a>
+        </li>
+    </ul>
+    <p class="copyright">
+        Copyright GGshop @2022
+    </p>
+</section>
+</div>
